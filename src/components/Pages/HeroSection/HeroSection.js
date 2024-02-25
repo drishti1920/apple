8985bb8cde2bf20ed300b1e9ff2dcd74fea6bccc
Drishti Saraf <drishtisaraf0307@gmail.com>
2024-02-25 23:26:05 +0530

    Hero Section

--- src/components/Pages/HeroSection/HeroSection.js
+++ src/components/Pages/HeroSection/HeroSection.js
@@ -3,13 +3,23 @@ import classes from "./HeroSection.module.css"
 
 
 const HeroSection = () => {
+
+  const learnMore =()=>{
+    window.location.href = 'https://www.apple.com/iphone-15-pro/'
+  }
+
+  const buyNow = () => {
+    window.location.href = 'https://www.apple.com/shop/buy-iphone/iphone-15-pro'
+  }
+
+
   return (
     <div className={classes.MainSection}>
         <h1>iPhone 15 Pro</h1>
         <h5>Titanium. So strong. So light. So pro.</h5>
         <div className={classes.ctaLinks}>
-         <button>Learn more</button> 
-         <button>Buy</button> 
+         <button onClick={learnMore}>Learn more</button> 
+         <button onClick={buyNow}>Buy</button> 
         </div>
     </div>
   )
